c44624d6334f8660785e9b96cb0e3caf488151b0
“Thomas <tjwhitephd@gmail.com>
2024-10-31 18:36:35 -0700

    fixed bugs and built delete route thanks to Josh Naylor

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,16 @@
+const path = require("path");
 // Imports fs and call the Router method
-
 const fs = require("fs");
-
 // Imports express and call the Router method
 const express = require("express");
 const apiSubrouter = express.Router();
 
-// TODO: Use get and post routes
+// build path to notes JSON file
+const dbFilePath = path.join(__dirname, "..", "db", "db.json");
+
+// get route
 apiSubrouter.get("/notes", (req, res) => {
-  fs.readFile("./db/db.json", "UTF-8", (error, data) => {
+  fs.readFile(dbFilePath, "UTF-8", (error, data) => {
     if (error) {
       console.log(error);
     } else {
@@ -18,24 +20,48 @@ apiSubrouter.get("/notes", (req, res) => {
   });
 });
 
+// post route
 apiSubrouter.post("/notes", (req, res) => {
-  fs.readFile("./db/db.json", "UTF-8", (error, data) => {
+  fs.readFile(dbFilePath, "UTF-8", (error, data) => {
     if (error) {
       console.log(error);
     } else {
       const notes = JSON.parse(data);
+      console.log(notes);
       const newNote = req.body;
-      notes.push (newNote);
-    fs.writeFile("./db/db.json", JSON.stringify(newNote), (error) => {
+      newNote.id = Math.random();
+      console.log(id);
+      notes.push(newNote);
+      fs.writeFile(dbFilePath, JSON.stringify(notes), (error) => {
         if (error) {
-            console.log(error);
+          console.log(error);
         } else {
-            // todo: make this writeFile work
+          res.status(200).json(notes);
         }
-    }
+      });
     }
   });
 });
 
-// TODO: Export app
+apiSubrouter.delete("/notes/:id", (req, res) => {
+  fs.readFile(dbFilePath, "UTF-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const notes = JSON.parse(data);
+      console.log(notes);
+      console.log(req.params);
+
+      // todo: turn req.params.id into a number, then remove note from array by ID.
+      fs.writeFile(dbFilePath, JSON.stringify(notes), (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.status(200).json(notes);
+        }
+      });
+    }
+  });
+});
+// Exports app
 module.exports = apiSubrouter;
